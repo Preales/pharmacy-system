f9392e26a8c53c11f3e66fe5647d1f01ac67a9f8
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2026-08-28 18:35:34 -0500

    feat(fe): add i18n spanish translations and es-CO locale

--- src/frontend/pharmacy-frontend/src/app/app.config.ts
+++ src/frontend/pharmacy-frontend/src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { registerLocaleData } from '@angular/common';
+import localeEsCO from '@angular/common/locales/es-CO';
 import Material from '@primeng/themes/material';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { tenantInterceptor } from './core/interceptors/tenant.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+
+registerLocaleData(localeEsCO);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,5 +30,6 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
+    { provide: LOCALE_ID, useValue: 'es-CO' },
   ],
 };
